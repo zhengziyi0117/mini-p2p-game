177f6e3588d913519e2480f4c2f40fcebb54b5b7
zhengziyi0117 <91662408+zhengziyi0117@users.noreply.github.com>
2026-09-19 18:58:17 +0800

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/mini-p2p-game/",
+  plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
+});
